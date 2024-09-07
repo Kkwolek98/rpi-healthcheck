d97f7d0af0b847febd796b95c50bab10b3d1541a
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-09-07 22:05:12 +0200

    fixed mock vcgencmd, setup-mock.sh

--- mock-commands/vcgencmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$1" == "measure_temp" ]; then
-    rand = awk -v min=45 -v max=60 'BEGIN{srand(); printf "%.2f\n", min+rand()*(max-min)}'
-    echo "temp=$rand'C"
-    exit 0
-fi
-
-echo "Error: unsupported command: $1"
-exit 1
