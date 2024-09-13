b62f4c8e3df816e09826cad1d721838fca5103c7
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-09-13 22:28:39 +0200

    average out temperature readouts

--- server/db/temperatures.go
+++ server/db/temperatures.go
@@ -1,6 +1,9 @@
 package db
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type TemperatureReadout struct {
 	Temp      float64   `json:"temp"`
@@ -25,5 +28,51 @@ func GetLastWeekTemperatureReadings() ([]TemperatureReadout, error) {
 		return nil, r.Error
 	}
 
-	return readouts, nil
+	avgReadouts := averageOutReadouts(&readouts, 8)
+
+	return avgReadouts, nil
+}
+
+func averageOutReadouts(readouts *[]TemperatureReadout, sections int) []TemperatureReadout {
+	if len(*readouts) == 0 || sections <= 0 {
+		return nil
+	}
+
+	readoutsLen := len(*readouts)
+	sectionSize := readoutsLen / sections
+
+	if sectionSize <= 1 {
+		return *readouts
+	}
+
+	var averagedReadouts []TemperatureReadout
+
+	for i := 0; i < sections; i++ {
+		start := i * sectionSize
+		end := start + sectionSize
+
+		if i == sections-1 {
+			end = readoutsLen
+		}
+
+		currSection := (*readouts)[start:end]
+		currSectionLen := len(currSection)
+
+		var totalTemp float64
+		for _, r := range currSection {
+			totalTemp += r.Temp
+		}
+		avgTemp := totalTemp / float64(currSectionLen)
+		roundedAvgTemp := math.Round(avgTemp*100) / 100
+
+		midIndex := (start + currSectionLen) / 2
+		midTimestamp := (*readouts)[midIndex].Timestamp
+
+		averagedReadouts = append(averagedReadouts, TemperatureReadout{
+			Temp:      roundedAvgTemp,
+			Timestamp: midTimestamp,
+		})
+	}
+
+	return averagedReadouts
 }
